79e164f8b7efa04bf6a824d2e45a99a287021016
HNVN-dev <hoainamvalentinnguyen@gmail.com>
2024-01-28 15:10:38 +0100

    Init: Project

--- src/Primary-Adapters/React/App.tsx
+++ src/Primary-Adapters/React/App.tsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+
 function App() {
+  const [text, setText] = useState("React Redux Clean architecture starter");
+
   return (
     <>
-      <h1>React Redux Clean architecture starter</h1>
+      <h1>Test</h1>
+      <div>
+        <p>{text}</p>
+        <input />
+      </div>
     </>
   );
 }
